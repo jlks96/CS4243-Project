7e797069554240b09986cc77cadea7d022cc42f3
jlks96 <joshlee_1996@hotmail.com>
2019-09-22 23:28:24 +0800

    editing detector.py (allowing argv to allow user specify classifier and image_path)

--- cascadeclassifier/detector.py
+++ cascadeclassifier/detector.py
@@ -1,18 +1,19 @@
 import cv2
 import os
+import sys
 
-def detectWaldo(image_path):
-    img = cv2.imread(image_path)
+classifier = sys.argv[1]
+image_path = sys.argv[2]
 
-    cascade = cv2.CascadeClassifier("classifiers\\cascade.xml")
+img = cv2.imread(image_path)
 
-    waldos = cascade.detectMultiScale(img, scaleFactor=1.105, minNeighbors=18, minSize=(25, 25), maxSize=(50,50))
+cascade = cv2.CascadeClassifier(classifier)
 
-    for (i, (x, y, w, h)) in enumerate(waldos):
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
-        cv2.putText(img, "Waldo detected!", (x - 100, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
+waldos = cascade.detectMultiScale(img, minNeighbors=18)
 
-    cv2.imshow("Where is Waldo?", img)
-    cv2.waitKey(0)
+for (i, (x, y, w, h)) in enumerate(waldos):
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+    cv2.putText(img, "Waldo detected!", (x - 100, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
 
-detectWaldo("original_images\\8.jpg")
+cv2.imshow("Where is Waldo?", img)
+cv2.waitKey(0)
