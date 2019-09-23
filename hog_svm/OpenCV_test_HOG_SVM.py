d820cd0d5a3577e0f376fb2aa6fd984a2324f736
jlks96 <joshlee_1996@hotmail.com>
2019-09-23 22:07:20 +0800

    adding HOG SVM (OpenCV version)

---
+++ hog_svm/OpenCV_test_HOG_SVM.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import sys
+import os
+import pickle
+import glob
+
+def inside(r, q):
+    rx, ry, rw, rh = r
+    qx, qy, qw, qh = q
+    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+
+def draw_detections(img, rects, thickness = 1):
+    for x, y, w, h in rects:
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 0, 255), thickness)
+
+svm = pickle.load(open("svm.pickle", "rb"))
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(np.array(svm))
+
+img = cv2.imread("original_images\\8.jpg")
+
+print("Starting detection...")
+found, w = hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.1)
+found_filtered = []
+for ri, r in enumerate(found):
+    for qi, q in enumerate(found):
+        if ri != qi and inside(r, q):
+            break
+        else:
+            found_filtered.append(r)
+    draw_detections(img, found)
+    draw_detections(img, found_filtered, 3)
+    print('%d (%d) found' % (len(found_filtered), len(found)))
+cv2.imshow('img', img)
+
+cv2.waitKey(0) & 0xFF
+cv2.destroyAllWindows()
---
+++ hog_svm/OpenCV_train_HOG_SVM.py
@@ -0,0 +1,54 @@
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import cv2
+import pickle
+import re
+from skimage.feature import hog
+from PIL import Image
+from cv2 import ml
+
+# Parameters of HOG feature extraction
+orientations = 9
+pixels_per_cell = (8, 8)
+cells_per_block = (2, 2)
+threshold = .3
+
+pos_im_path = "pos_data"
+neg_im_path= "neg_data"
+
+pos_im_listing = os.listdir(pos_im_path)
+neg_im_listing = os.listdir(neg_im_path)
+data= []
+labels = []
+
+for file in pos_im_listing:
+    img = Image.open(pos_im_path + '\\' + file)
+    img = img.resize((64, 128))
+    gray = img.convert('L')
+    fd = hog(gray, orientations, pixels_per_cell, cells_per_block, block_norm='L2', feature_vector=True)
+    data.append(fd)
+    labels.append(1)
+
+for file in neg_im_listing:
+    img= Image.open(neg_im_path + '\\' + file)
+    img = img.resize((64, 128))
+    gray= img.convert('L')
+    fd = hog(gray, orientations, pixels_per_cell, cells_per_block, block_norm='L2', feature_vector=True) 
+    data.append(fd)
+    labels.append(0)
+
+svm = cv2.ml.SVM_create()
+svm.setType(cv2.ml.SVM_C_SVC)
+svm.setKernel(cv2.ml.SVM_LINEAR)
+svm.train(np.array(data, dtype=np.float32), cv2.ml.ROW_SAMPLE, np.array(labels, dtype=np.int32))
+
+svm.save("svm.xml")
+tree = ET.parse('svm.xml')
+root = tree.getroot()
+
+SVs = root.getchildren()[0].getchildren()[-2].getchildren()[0] 
+rho = float( root.getchildren()[0].getchildren()[-1].getchildren()[0].getchildren()[1].text )
+svmvec = [float(x) for x in re.sub( '\s+', ' ', SVs.text ).strip().split(' ')]
+svmvec.append(-rho)
+pickle.dump(svmvec, open("svm.pickle", 'wb'))
