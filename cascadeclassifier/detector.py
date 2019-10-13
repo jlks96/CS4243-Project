700ae2b79ba0aeb20084fb512ef2f7201abbe139
airine <airine0119@gmail.com>
2019-10-13 16:36:15 +0800

    update template matching

--- cascadeclassifier/detector.py
+++ cascadeclassifier/detector.py
@@ -8,28 +8,29 @@ image_path = sys.argv[2]
 image_idx = sys.argv[3]
 output_path = sys.argv[4]
 
-bl = open(os.path.join(output_path, "baseline.txt"), "a")
+# bl = open(os.path.join(output_path, "baseline.txt"), "a")
+with open(os.path.join(output_path, "baseline.txt"), "a") as bl:
+    img = cv2.imread(image_path)
+    cascade = cv2.CascadeClassifier(classifier)
+    waldos, rejectLevels, levelWeights = cascade.detectMultiScale3(img, scaleFactor=1.01, outputRejectLevels=True)
 
-img = cv2.imread(image_path)
-cascade = cv2.CascadeClassifier(classifier)
-waldos, rejectLevels, levelWeights = cascade.detectMultiScale3(img, scaleFactor=1.01, outputRejectLevels=True)
+    for (i, ((x, y, w, h), levelWeight)) in enumerate(zip(waldos, levelWeights)):
+        # Computes confidence score
+        if levelWeight[0] >= 0:
+            z = math.exp(-levelWeight[0])
+            confidence_score = 1 / (1 + z)
+        else:
+            z = math.exp(levelWeight[0])
+            confidence_score = z / (1 + z)
 
-for (i, ((x, y, w, h), levelWeight)) in enumerate(zip(waldos, levelWeights)):
-    # Computes confidence score
-    if levelWeight[0] >= 0:
-        z = math.exp(-levelWeight[0])
-        confidence_score = 1 / (1 + z)
-    else:
-        z = math.exp(levelWeight[0])
-        confidence_score = z / (1 + z)
+        # Outputs to baseline.txt
+        bl.write(" ".join(map(str, [image_idx, confidence_score, x, y, x + w, y + h])) + "\n")
 
-    # Outputs to baseline.txt
-    bl.write(" ".join(map(str, [image_idx, confidence_score, x, y, x + w, y + h])) + "\n")
+        # Visualisation
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+        cv2.putText(img, str(confidence_score), (x, y), cv2.FONT_ITALIC, 0.7, (255,0,0), 2)
+    bl.close()
+    cv2.namedWindow("Where is Waldo?", cv2.WINDOW_NORMAL)
+    cv2.imshow("Where is Waldo?", img)
+    cv2.waitKey(0)
 
-    # Visualisation
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
-    cv2.putText(img, str(confidence_score), (x, y), cv2.FONT_ITALIC, 0.7, (255,0,0), 2)
-
-cv2.namedWindow("Where is Waldo?", cv2.WINDOW_NORMAL)
-cv2.imshow("Where is Waldo?", img)
-cv2.waitKey(0)
--- template_matching/pure_tm.py
+++ template_matching/pure_tm.py
@@ -8,32 +8,36 @@ from platform import system
 import os
 
 sp = '\\' if str(system()) == 'Windows' else '/'
-t_path = 'templates/waldo/head/004_0_head.jpg'
-threshold = 0.7
+# t_path = 'templates/waldo/head/004_0_head.jpg'
+ts_path = 'template_matching/templates/selected/single'
+threshold = 0.5
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--template", help="Path to template image")
 ap.add_argument("-i", "--image", required=True,
 	help="Path to image where template will be matched")
 args = vars(ap.parse_args())
-t_path = args["template"] if args["template"] else t_path
-template = cv2.imread(t_path)
-template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-# template = cv2.Canny(template, 50, 200)
-(tH, tW) = template.shape[:2]
+ts_path = args["template"] if args["template"] else t_path
+# template = cv2.imread(t_path)
+# template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+# # template = cv2.Canny(template, 50, 200)
+# (tH, tW) = template.shape[:2]
+# print(tH/tW)
 # cv2.imshow("Template", template)
 # cv2.waitKey(0)
 
 image_path = args["image"]
 
 target_name = os.path.basename(image_path)[:-4]
-templ_name = os.path.basename(t_path)[:-4]
+templ_name = os.path.basename(ts_path)
+
 # for folder
 # for imagePath in glob.glob(args["images"] + "/*.jpg"):
 
 image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-print(gray.shape)
+gray = cv2.Canny(gray, 50, 200)
+# print(gray.shape)
 # edged = cv2.Canny(gray, 50, 200)
 # edged = gray
 # cv2.imshow("Image", edged)
@@ -41,43 +45,53 @@ print(gray.shape)
 
 boxes = list()
 
-scale = 0.0372
-for scale in tqdm(np.linspace(0.02, 0.3, 40)[::-1]):
-	# print(scale)
-# loop over the scales of the image
-# for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-# for scale in np.linspace(0.5, 10.0, 20)[::-1]:
-# scale = 6
-# resize the image according to the scale, and keep track
-# of the ratio of the resizing
-	resized_tp = utils.resize(template, width = int(gray.shape[1] * scale))
-# resized_tp = cv2.Canny(resized_tp, 50, 200)
-# cv2.imshow("Image", resized_tp)
-# cv2.waitKey(0)
-# r = gray.shape[1] / float(resized.shape[1])
-# print('resize ratio:', r)
+# t_path = args["template"] if args["template"] else t_path
+for t_path in glob.glob(ts_path + "/*.jpg"):
+	template = cv2.imread(t_path)
+	template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+	# template = cv2.Canny(template, 50, 200)
+	# (tH, tW) = template.shape[:2]
 
-# if the resized image is smaller than the template, then break
-# from the loop
-# if resized.shape[0] < tH or resized.shape[1] < tW:
-# 	print('!!!The image is smaller than the template!!!')
-	# break
+	for scale in tqdm(np.linspace(0.02, 0.25, 40)[::-1]):
+		# print(scale)
+		# loop over the scales of the image
+		# for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+		# for scale in np.linspace(0.5, 10.0, 20)[::-1]:
+		resized_tp = utils.resize(template, width = int(gray.shape[1] * scale))
+		resized_tp = cv2.Canny(resized_tp, 50, 200)
+		tp_shape = resized_tp.shape[:2]
+		# print(tp_shape[0]/tp_shape[1])
+		
+		# cv2.imshow("Image", resized_tp)
+		# key = cv2.waitKey(0)
+		# if (key == ord('q')):
+		# 	break
+		# r = gray.shape[1] / float(resized.shape[1])
+		# print('resize ratio:', r)
 
-# detect edges in the resized, grayscale image and apply template
-# matching to find the template in the image
-# edged = cv2.Canny(resized, 50, 200)
-# print('start')
-	res = cv2.matchTemplate(gray, resized_tp, cv2.TM_CCOEFF_NORMED)
-# print('finish')
-	loc = np.where(res >= threshold)
-# print(len(loc))
+		# if the resized image is smaller than the template, then break
+		# from the loop
+		# if resized.shape[0] < tH or resized.shape[1] < tW:
+		# 	print('!!!The image is smaller than the template!!!')
+			# break
 
-	for pt in zip(*loc[::-1]):
-		# TODO: avoid overlapping (kMeans or Meanshift)
-		boxes.append((pt[0], pt[1], scale))
+		# detect edges in the resized, grayscale image and apply template
+		# matching to find the template in the image
+		# edged = cv2.Canny(resized, 50, 200)
+		res = cv2.matchTemplate(gray, resized_tp, cv2.TM_CCOEFF_NORMED)
 
-print(boxes)
+		loc = np.where(res >= threshold)
+		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(res)
+		if maxVal > 0.1:
+			boxes.append((maxLoc[0], maxLoc[1], maxVal, scale, tp_shape))
+		# for pt in zip(*loc[::-1]):
+		# 	# TODO: avoid overlapping (kMeans or Meanshift)
+		# 	print((pt[0], pt[1], scale))
+		# 	boxes.append((pt[0], pt[1], scale, ))
+	# print(res)
+# print(boxes)
+
 for b in boxes:
-	cv2.rectangle(image, (b[0],b[1]), (b[0]+int(scale*gray.shape[1]),b[1]+int(scale*gray.shape[0])), (255,0,0), 2)
-
+	cv2.rectangle(image, (b[0],b[1]), (b[0]+b[4][1],b[1]+b[4][0]), (255,0,0), 2)
+	cv2.putText(image, 'score:%.3f, scale:%.3f' % (b[2],b[3]), (b[0],b[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
 cv2.imwrite('results'+sp+target_name+'_'+templ_name+'.jpg',image)
