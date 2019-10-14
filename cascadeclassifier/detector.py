b1622fc668706465d13a7136048d8fd34fa01213
jlks96 <joshlee_1996@hotmail.com>
2019-10-15 02:30:28 +0800

    updating detector.py

--- cascadeclassifier/detector.py
+++ cascadeclassifier/detector.py
@@ -8,33 +8,28 @@ image_path = sys.argv[2]
 image_idx = sys.argv[3]
 output_path = sys.argv[4]
 
-bl = open(os.path.join(output_path, "baseline.txt"), "a")
+with open(os.path.join(output_path, "baseline.txt"), "a") as bl:
+    img = cv2.imread(image_path)
+    cascade = cv2.CascadeClassifier(classifier)
+    waldos, rejectLevels, levelWeights = cascade.detectMultiScale3(img, scaleFactor=1.05, outputRejectLevels=True)
 
-img = cv2.imread(image_path)
-cascade = cv2.CascadeClassifier(classifier)
-waldos, rejectLevels, levelWeights = cascade.detectMultiScale3(img, scaleFactor=1.05, outputRejectLevels=True)
+    for (i, ((x, y, w, h), levelWeight)) in enumerate(zip(waldos, levelWeights)):
+        # Computes confidence score
+        if levelWeight[0] >= 0:
+            z = math.exp(-levelWeight[0])
+            confidence_score = 1 / (1 + z)
+        else:
+            z = math.exp(levelWeight[0])
+            confidence_score = z / (1 + z)
 
-results = []
+        # Outputs to baseline.txt
+        bl.write(" ".join(map(str, [image_idx, confidence_score, x, y, x + w, y + h])) + "\n")
 
-for (i, ((x, y, w, h), levelWeight)) in enumerate(zip(waldos, levelWeights)):
-    # Computes confidence score
-    if levelWeight[0] >= 0:
-        z = math.exp(-levelWeight[0])
-        confidence_score = 1 / (1 + z)
-    else:
-        z = math.exp(levelWeight[0])
-        confidence_score = z / (1 + z)
+        # Visualisation
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+        cv2.putText(img, str(confidence_score), (x, y), cv2.FONT_ITALIC, 0.7, (255,0,0), 2)
 
-    # Adds to results list
-    results.append((image_idx, confidence_score, x, y, x + w, y + h))
-
-    # Visualisation
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
-    cv2.putText(img, str(confidence_score), (x, y), cv2.FONT_ITALIC, 0.7, (255,0,0), 2)
-
-# results.sort(key=lambda tup: tup[1], reverse=True)
-for result in results:
-    bl.write(" ".join(map(str, result)) + "\n")
+bl.close()
 
 cv2.namedWindow("Where is Waldo?", cv2.WINDOW_NORMAL)
 cv2.imshow("Where is Waldo?", img)
