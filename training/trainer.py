1ccbbaa565dcacc79273ba798f1cb500e1160c64
jlks96 <joshlee_1996@hotmail.com>
2019-10-16 14:07:28 +0800

    modifying trainer (generate as many pos eg for pos.vec, change naming convention of folder)

--- training/trainer.py
+++ training/trainer.py
@@ -15,19 +15,21 @@ def train(w, bt, min_hit_rate, max_false_alarm_rate, mode, num_pos, num_neg, k):
                 # Determine paths
                 data_folder = os.path.join("data", str(i), character, part)
                 info_path = os.path.join(data_folder, "info.dat")
-                pos_vec_path = os.path.join(data_folder, "pos.vec")
+                pos_vec_path = os.path.join(data_folder, w, "pos.vec")
                 bg_path = os.path.join(data_folder, "bg.txt")
 
-                # Create positive examples
+                # Create positive examples (pos.vec)
+                info = open(info_path, "r")
+                num_posv = len(info.readlines()) # Create as many pos for pos.vec as in info.dat
                 create_samples_cmd = "opencv_createsamples -info {} -num {} -w {} -h {} -vec {}".format(
-                    info_path, num_pos, w, h, pos_vec_path)
+                    info_path, num_posv, w, h, pos_vec_path)
                 os.system(create_samples_cmd)
 
                 # Train cascade classifier
                 for num_stage in range(10, 21): # Iterate from 10 to 20 stages
-                    # folder name is "numStage_h_w_bt_minHitRate_maxFalseAlarmRate_mode"
-                    model_folder = os.path.join("trained_models", "{}_{}_{}_{}_{}_{}_{}".format(
-                        num_stage, h, w, bt, min_hit_rate, max_false_alarm_rate, mode), str(i), character, part)
+                    # folder name is "numStage_w_bt_minHitRate_maxFalseAlarmRate_mode"
+                    model_folder = os.path.join("trained_models", "{}_{}_{}_{}_{}_{}".format(
+                        num_stage, w, bt, min_hit_rate, max_false_alarm_rate, mode), str(i), character, part)
 
                     if not os.path.exists(model_folder):
                         os.makedirs(model_folder)
