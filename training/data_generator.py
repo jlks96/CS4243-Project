270e4fde5635cdb5bfe6b49138c67a2358bbfdca
jlks96 <joshlee_1996@hotmail.com>
2019-10-20 15:33:44 +0800

    updating k and parts in data_generator

--- training/data_generator.py
+++ training/data_generator.py
@@ -54,7 +54,7 @@ def generate_training_validation_set(training_validation_set, k):
 
 if __name__ == "__main__":
     # Constants
-    _k = 10 # k-fold cross validation
+    _k = 5 # k-fold cross validation
     _training_txt_path = os.path.join("..", "datasets", "ImageSets", "train.txt") # Dir of train.txt
     _neg_path = os.path.join("..", "datasets", "NegativeSamples") # Dir of NegativeSamples
     _pos_path = os.path.join("..", "datasets", "PositiveSamples") # Dir of PositiveSamples
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
     # Generate bg and info files for all k training sets
     for character in ["waldo", "wenda", "wizard"]:
-        for part in ["body", "full", "head"]:
+        for part in ["full", "head"]:
             char_part_pos_path = os.path.join(_pos_path, character, part)
             for i in range(_k):
                 storage_path = os.path.join("data", str(i), character, part)
