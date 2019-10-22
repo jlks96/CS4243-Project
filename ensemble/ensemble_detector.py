ae257fe2eb68c1848ba4c91318e8f4c9b39ba1a5
airine <airine0119@gmail.com>
2019-10-23 00:33:52 +0800

    some update

--- ensemble/ensemble_detector.py
+++ ensemble/ensemble_detector.py
@@ -2,7 +2,12 @@ import cv2
 import os
 import sys
 import math
+import scipy
+import glob
+from utils import resize
 from argparse import ArgumentParser
+from cv_utils import template_matching as tm
+from cv_utils import feature_extractor as fe
 
 def cascade_classify(classifier, image_path, image_idx):
     # Store list of detection results by cascade classifier
@@ -30,10 +35,43 @@ def cascade_classify(classifier, image_path, image_idx):
 
     return results
 
-def template_matching(prelim_results, output_path):
+def template_matching(prelim_results, image_path, ts_path, output_path, option='hog', distance = 'correlation', match_size=50):
+    # Option can be in ['hog', 'basic', 'multi']
+    t_paths = list(glob.glob(ts_path + "/*.jpg"))
+    feature = fe.factory(option)
     with open(os.path.join(output_path, "baseline.txt"), "a") as bl:
-
+        image = cv2.imread(image_path)
         # Template matching operation
+        # For every patch detected
+        for pr in prelim_results:
+            print(pr)
+            # xleft, xright, ytop, tbottom
+            xl, xr, yt, yb = pr[2], pr[4], pr[3], pr[5]
+            # crop the patch
+            patch = image[yt:yb,xl:xr]
+            # For evert template
+            dis = 0.0
+            for t_path in t_paths:
+                template = cv2.imread(t_path)
+
+                patch = resize(patch, width=match_size, height=match_size)
+                template = resize(template, width=match_size, height=match_size)
+                print(patch.shape)
+                print(template.shape)
+
+                p_feature = feature(patch)
+                t_feature = feature(template)
+                print(patch.shape)
+                print(template.shape)
+
+                if distance == 'euclidean':
+                    dis = scipy.spatial.distance.euclidean(t_feature.flatten(), p_feature.flatten())
+                elif distance == 'correlation':
+                    dis = scipy.spatial.distance.correlation(t_feature.flatten(), p_feature.flatten())
+                # print(heatmap)
+                print(dis)
+                return
+
         pass
 
     bl.close()
@@ -45,7 +83,12 @@ if __name__ == "__main__":
     parser.add_argument('-ip', '--img_path', required=True, help="image input path")
     parser.add_argument('-ii', '--img_idx', required=True, help="image index")
     parser.add_argument('-op', '--output_path', required=True, help="baseline output path")
+    parser.add_argument('-tp', '--tp_path', required=True, help="templates folder path")
     args = parser.parse_args()
 
-    prelim_results = cascade_classify(args.classifier, args.image_path, args.image_idx)
-    template_matching(prelim_results, args.output_path)
+    prelim_results = cascade_classify(args.classifier, args.img_path, args.img_idx)
+    # img = cv2.imread(args.img_path)
+    # cv2.imshow("test",img[1384:1465,2144:2225])
+    # cv2.waitKey(0)
+    template_matching(prelim_results, args.img_path, args.tp_path, args.output_path)
+    # 011 0.7938499034133966 2144 1384 2225 1465
--- template_matching/basic_template_matching.py
+++ template_matching/basic_template_matching.py
@@ -9,10 +9,10 @@ import os
 
 sp = '\\' if str(system()) == 'Windows' else '/'
 # t_path = 'templates/waldo/head/004_0_head.jpg'
-ts_path = 'templates/selected/single'
 # ts_path = 'templates/selected/single'
-threshold = 0.1
-min_scale = 0.005
+ts_path = 'templates/selected/waldo'
+threshold = 0.2
+min_scale = 0.01
 max_scale = 0.05
 # 032 min: 0.02, max: 0.25
 iterations = 20
@@ -29,38 +29,47 @@ image_path = '../datasets/JPEGImages/'+args["image"]+'.jpg'
 target_name = os.path.basename(image_path)[:-4]
 templ_name = os.path.basename(ts_path)
 
-print(image_path)
+# print(image_path)
 image = cv2.imread(image_path)
+image = utils.resize(image, width = 2048)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.Canny(gray, 50, 200)
+# gray = cv2.Canny(gray, 50, 200)
 
 boxes = list()
 
 # t_path = args["template"] if args["template"] else t_path
 t_paths = list(glob.glob(ts_path + "/*.jpg"))
-print(t_paths)
-for t_path in t_paths:
+# print(t_paths)
+
+print(len(t_paths), 'templates used.')
+for t_path in tqdm(t_paths):
+	# print('Template: ', t_path)
 	template = cv2.imread(t_path)
 	template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 	# template = cv2.Canny(template, 50, 200)
 	# (tH, tW) = template.shape[:2]
 	name = os.path.basename(t_path)[:-4]
-
-	for scale in tqdm(np.linspace(min_scale, max_scale, iterations)[::-1]):
+	t_max = 0
+	box = None
+	for scale in np.linspace(min_scale, max_scale, iterations)[::-1]:
 
 		resized_tp = utils.resize(template, width = int(gray.shape[1] * scale))
-		resized_tp = cv2.Canny(resized_tp, 50, 200)
+		# resized_tp = cv2.Canny(resized_tp, 50, 200)
 		tp_shape = resized_tp.shape[:2]
 
 		res = cv2.matchTemplate(gray, resized_tp, cv2.TM_CCOEFF_NORMED)
 
 		loc = np.where(res >= threshold)
 		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(res)
-		if maxVal > 0.1:
-			boxes.append((maxLoc[0], maxLoc[1], maxVal, scale, tp_shape, name))
+		if maxVal > t_max:
+			box = (maxLoc[0], maxLoc[1], maxVal, scale, tp_shape, name)
+		# if maxVal > 0.1:
+		# 	boxes.append((maxLoc[0], maxLoc[1], maxVal, scale, tp_shape, name))
+	# if t_max > threshold:
+	boxes.append(box)
 
 for b in boxes:
 	cv2.rectangle(image, (b[0],b[1]), (b[0]+b[4][1],b[1]+b[4][0]), (255,0,0), 2)
-	cv2.putText(image, 'score:%.3f, scale:%.3f, name:%s' % (b[2],b[3],b[5]), (b[0],b[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
+	# cv2.putText(image, 'score:%.3f, scale:%.3f, name:%s' % (b[2],b[3],b[5]), (b[0],b[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
 cv2.imwrite('results'+sp+target_name+'_'+templ_name+'.jpg',image)
