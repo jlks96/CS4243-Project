879e0d602829bd449a04e6801cd6762576274c10
jlks96 <joshlee_1996@hotmail.com>
2019-10-28 22:43:10 +0800

    creating comprehensive ensemble_detector

--- ensemble/ensemble_detector.py
+++ ensemble/ensemble_detector.py
@@ -4,103 +4,110 @@ import sys
 import math
 import scipy
 import glob
+import skimage.feature
 import numpy as np
-from tqdm import tqdm
 from argparse import ArgumentParser
-import feature_extractor as fe
 
-def cascade_classify(classifier, image_path, image_idx):
+def cascade_classify(classifier_path, image_path):
     # Store list of detection results by cascade classifier
-    # Format of detection: (image index, confidence_score, x1, y1, x2, y2)
+    # Format of detection: (x1, y1, x2, y2)
     results = []
 
     # Read in image and classifier
     img = cv2.imread(image_path)
-    cascade = cv2.CascadeClassifier(classifier)
+    cascade = cv2.CascadeClassifier(classifier_path)
 
     # Cascade multiscale detection
-    detections, _, levelWeights = cascade.detectMultiScale3(img, scaleFactor=1.05, outputRejectLevels=True)
+    detections = cascade.detectMultiScale(img, scaleFactor=1.05)
 
-    for (x, y, w, h), levelWeight in zip(detections, levelWeights):
-        # Compute confidence score using sigmoid function
-        if levelWeight[0] >= 0:
-            z = math.exp(-levelWeight[0])
-            confidence_score = 1 / (1 + z)
-        else:
-            z = math.exp(levelWeight[0])
-            confidence_score = z / (1 + z)
-
-        # Output to baseline.txt
-        results.append((image_idx, confidence_score, x, y, x + w, y + h))
+    for (x, y, w, h) in detections:
+        # Output to results list
+        results.append((x, y, x + w, y + h))
 
     return results
 
-def template_matching(prelim_results, image_path, ts_path, output_path, option='hog', distance = 'correlation', match_size=50):
-    # Option can be in ['hog', 'rgb', 'gray','']
-    # Distance can be in ['euclidean', 'correlaion']
-    t_paths = list(glob.glob(ts_path + "/*.jpg"))
-    feature = fe.factory(option)
-    if distance == 'correlation':
-        get_dis = scipy.spatial.distance.correlation
-    else:
-        get_dis = scipy.spatial.distance.euclidean
+def template_matching(prelim_results, image_path, image_idx, template_folder, baseline_path, size=50):
+    # Get all template paths in template folder
+    template_paths = list(glob.glob(template_folder + "/*.jpg"))
+
+    # Use correlation distance as the metric
+    dist_metric = scipy.spatial.distance.correlation
+    
     image = cv2.imread(image_path)
-    with open(os.path.join(output_path, "baseline.txt"), "a") as bl:
+
+    with open(baseline_path, "a") as bl:
         # Template matching operation
-        tm_result = []
-        # For every patch detected
-        for pr in tqdm(prelim_results):
-            # xleft, xright, ytop, tbottom
-            xl, xr, yt, yb = pr[2], pr[4], pr[3], pr[5]
-            # crop the patch
-            patch = image[yt:yb,xl:xr]
-            # For evert template
-            diss = []
-            for t_path in t_paths:
-                template = cv2.imread(t_path)
+        # List of template matching distances for each patch
+        results = []
+        
+        # For every patch in prelim_results
+        for x1, y1, x2, y2 in prelim_results:
+            # Crop the patch
+            patch = image[y1:y2, x1:x2]
+            
+            # List of distances between current patch and each of the templates
+            dists = []
+            # For every template in template paths
+            for tp in template_paths:
+                template = cv2.imread(tp)
 
-                patch = cv2.resize(patch, (match_size, match_size), interpolation = cv2.INTER_AREA)
-                template = cv2.resize(template, (match_size, match_size), interpolation = cv2.INTER_AREA)
+                # Resize patch and template to the same size
+                patch = cv2.resize(patch, (size, size), interpolation = cv2.INTER_AREA)
+                template = cv2.resize(template, (size, size), interpolation = cv2.INTER_AREA)
 
-                p_feature = feature(patch)
-                t_feature = feature(template)
+                # Get HOG features for patch and templates using grayscale
+                p_feature = skimage.feature.hog(cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY), orientations=9, 
+                                                pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
+                t_feature = skimage.feature.hog(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), orientations=9, 
+                                                pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
 
-                dis = get_dis(t_feature.flatten(), p_feature.flatten())
-                diss.append(dis)
-            diss = np.array(diss)
-            tm_result.append((np.mean(diss), np.min(diss)))
+                dist = dist_metric(t_feature, p_feature)
+                dists.append(dist)
+            
+            # Use the minimum (best) distance for the current patch as the result and add to results list
+            results.append(min(dists))
 
-        # score = np.array([tr[-1]+tr[-2]*0.5 for tr in tm_result])
-        score = np.array([tr[-1] for tr in tm_result])
-        score = (np.max(score)-score)/(np.max(score)-np.min(score))
+        results = np.array(results)
 
-        for (i, ((img_idx, _, x, y, xr, yb), tm_score)) in enumerate(zip(prelim_results, score)):
-            # print((img_idx, _, x, y, xr, yb), tm_score)
-            if (tm_score > 0.8):
-                # Outputs to baseline.txt
-                bl.write(" ".join(map(str, [img_idx, tm_score, x, y, xr, yb])) + "\n")
-                # Visualisation
-                cv2.rectangle(image, (x,y), (xr,yb), (255,0,0), 2)
-                cv2.putText(image, "%.3f" % tm_score, (x, y), cv2.FONT_ITALIC, 0.7, (255,0,0), 2)
+        # Compute scores for each patch from results array
+        # Formula: patch_score = (max_distance - patch_distance) / (max_distance - min_distance)
+        scores = (np.max(results) - results) / (np.max(results) - np.min(results))
+
+        for (x1, y1, x2, y2), score in zip(prelim_results, scores):
+            if (score > 0.6):
+                # Output to baseline
+                bl.write(" ".join(map(str, [image_idx, score, x1, y1, x2, y2])) + "\n")
 
     bl.close()
-    cv2.namedWindow("Where is Waldo?", cv2.WINDOW_NORMAL)
-    cv2.imshow("Where is Waldo?", image)
-    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Runner for ensemble detector.')
-    parser.add_argument('-c', '--classifier', required=True, help="classifier")
-    parser.add_argument('-ip', '--img_path', required=True, help="image input path")
-    parser.add_argument('-ii', '--img_idx', required=True, help="image index")
-    parser.add_argument('-op', '--output_path', required=True, help="baseline output path")
-    parser.add_argument('-tp', '--tp_path', required=True, help="templates folder path")
+    parser.add_argument('-ii', '--input_images', required=True, help="text files containing input image paths and indices")
     args = parser.parse_args()
 
-    prelim_results = cascade_classify(args.classifier, args.img_path, args.img_idx)
-    # img = cv2.imread(args.img_path)
-    # cv2.imshow("test",img[1384:1465,2144:2225])
-    # cv2.waitKey(0)
-    template_matching(prelim_results, args.img_path, args.tp_path, args.output_path)
-    # 011 0.7938499034133966 2144 1384 2225 1465
+    # Folder name constants
+    classifier_folder = "classifier"
+    output_baseline_folder = "baseline"
+    template_parent_folder = "template"
+
+    # Prepare input image paths and indices
+    image_paths_indices = [line.strip("\n").split(" ") for line in open(args.input_images)]
+
+    # Create output baseline folder if doesn't exist
+    if not os.path.exists(output_baseline_folder):
+        os.makedirs(output_baseline_folder)
+
+    # Start detections for all characters and body parts
+    for character in ["waldo", "wenda", "wizard"]:
+        for part in ["head", "full"]:
+            for image_path, image_idx in image_paths_indices:
+                classifier_path = os.path.join(classifier_folder, character, part, "cascade.xml")
+                baseline_path = os.path.join(output_baseline_folder, "{}.txt".format(character))
+                template_folder = os.path.join(template_parent_folder, character, part)
+
+                # First stage: cascade classifying
+                prelim_results = cascade_classify(classifier_path, image_path)
+                # Second stage: template matching
+                if (len(prelim_results) > 0):
+                    template_matching(prelim_results, image_path, image_idx, template_folder, baseline_path)
